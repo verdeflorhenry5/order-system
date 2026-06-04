4476c3d1bb22e2ad42ff665c659d38babdcb5597
verdeflorhenry5username <verdeflorhenry5email@gmail.com>
2026-06-04 13:59:13 +0800

    redirect root to dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return redirect('/dashboard');
+    });
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
